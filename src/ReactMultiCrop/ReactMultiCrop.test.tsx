03184d0b42bc4e324548a5741e6e36e8ead40923
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2020-10-18 17:09:58 +0700

    Update test, need to convert the test

--- src/ReactMultiCrop/ReactMultiCrop.test.tsx
+++ src/ReactMultiCrop/ReactMultiCrop.test.tsx
@@ -1,45 +1,14 @@
 import React from "react";
 import { render } from "@testing-library/react";
 
-import ReactMultiCrop from "./ReactMultiCrop";
-import { ReactMultiCropProps } from "./ReactMultiCrop.types";
+import ReactMultiCropForm from "./ReactMultiCropForm";
 
 describe("Test Component", () => {
-  let props: ReactMultiCropProps;
 
-  beforeEach(() => {
-    props = {
-      id: "canvas",
-      color: "red",
-      strokeColor: "white",
-      opacity: 0.8,
-      strokeDashArray: [5, 5],
-      strokeWidth: 5,
-      record: [],
-      input: {
-        value: "",
-        name: "react",
-        onChange: function () {},
-      },
-    };
-  });
+  const renderComponent = () => render(<ReactMultiCropForm />);
 
-  const renderComponent = () => render(<ReactMultiCrop {...props} />);
-
-  it("should have primary className with default props", () => {
-    const { getByTestId } = renderComponent();
-
-    const testComponent = getByTestId("canvas-wrapper");
-
-    expect(testComponent).toBeTruthy();
-  });
-
-  it("should have secondary className with theme set as secondary", () => {
-    props.id = "new-canvas";
-    const { getByTestId } = renderComponent();
-
-    const testComponent = getByTestId("canvas-wrapper");
-
-    expect(testComponent).toBeTruthy();
+  it("it should render correctly", () => {
+    const component = renderComponent();
+    expect(component).toBeTruthy();
   });
 });
